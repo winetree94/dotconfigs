2f72206d412fd6e2d001707583d7a0826d284b6e
winetree94 <me@winetree94.com>
2025-12-11 16:38:00 +0900

    update

--- roles/zsh/files/zsh/path.zsh
+++ roles/zsh/files/zsh/path.zsh
@@ -2,3 +2,4 @@ export PATH=/snap/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
 export VIVIDENT_KEYSTORE_DIR=$HOME/.vivident
+export GPG_TTY=$(tty)
