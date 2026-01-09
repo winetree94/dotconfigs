a89d97ce283ca69ab94306fbdb910162b3e4b402
winetree94 <me@winetree94.com>
2026-01-09 16:07:19 +0900

    터미널 단축키 설정 변경

---
+++ private_dot_config/nvim/empty_init.lua
@@ -0,0 +1,6 @@
+-- Tab을 Space로 변환
+vim.opt.expandtab = true
+
+vim.opt.tabstop = 2        -- Tab 문자를 표시할 때 사용할 칸 수
+vim.opt.shiftwidth = 2     -- 자동 indent 시 사용할 칸 수
+vim.opt.softtabstop = 2    -- Tab 키 누를 때 삽입할 칸 수
