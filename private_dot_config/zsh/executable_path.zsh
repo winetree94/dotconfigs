527a2c346e237f1bd43299f85ffd92459ffcbd36
winetree94 <me@winetree94.com>
2026-01-21 12:37:56 +0900

    feat: zsh path os 별 분기

--- private_dot_config/zsh/executable_path.zsh
+++
@@ -1,5 +0,0 @@
-export PATH=/snap/bin:$PATH
-export PATH=$HOME/.local/bin:$PATH
-export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
-export VIVIDENT_KEYSTORE_DIR=$HOME/.vivident
-export GPG_TTY=$(tty)
