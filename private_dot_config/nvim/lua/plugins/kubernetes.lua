640e45bed01a72a8ba8371a832b9443992a40db2
winetree94 <me@winetree94.com>
2026-02-11 22:58:44 +0900

    feat: neovim 설정 변경

--- private_dot_config/nvim/lua/plugins/kubernetes.lua
+++ private_dot_config/nvim/lua/plugins/kubernetes.lua
@@ -1,14 +1,16 @@
-MiniDeps.add({
-	source = "cwrau/yaml-schema-detect.nvim",
-	depends = {
-		"nvim-lua/plenary.nvim",
-	},
-})
+MiniDeps.later(function()
+	MiniDeps.add({
+		source = "cwrau/yaml-schema-detect.nvim",
+		depends = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
-require("yaml-schema-detect").setup({
-	keymap = {
-		refresh = "<leader>yr",
-		cleanup = "<leader>yc",
-		info = "<leader>yi",
-	},
-})
+	require("yaml-schema-detect").setup({
+		keymap = {
+			refresh = "<leader>yr",
+			cleanup = "<leader>yc",
+			info = "<leader>yi",
+		},
+	})
+end)
